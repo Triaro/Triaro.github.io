f0fdae26b1ad27d0a0c991288afb77e0eebe1e39
Triaro <abhimehra2762@gmail.com>
2025-03-01 22:02:20 +0530

    Added localStorage for Todos

--- src/components/Projects/ToDoApp/index.js
+++ src/components/Projects/ToDoApp/index.js
@@ -4,22 +4,27 @@ const TodoApp = () => {
     const [todos, setTodos] = useState([]);
     const [openInput, setOpenInput] = useState(false);
     const [loader, setLoader] = useState(true);
-    const [newTodoValue, setNewTodoValue] = useState('');
+    const [newTodo, setNewTodo] = useState('');
     useEffect(() => {
         getData();
-    }, []);
+    }, [localStorage]);
     const getData = () => {
         setLoader(true);
-        fetch('https://jsonplaceholder.typicode.com/todos')
-            .then(res => res.json())
-            .then(res => {
-                console.log(res);
-                res?.length && setTodos(res);
-                setLoader(false);
-            });
+        setTodos(JSON.parse(localStorage.getItem('todos')) || []);
+        setTimeout(() => {
+            setLoader(false);
+        }, 200);
+        // setLoader(true);
+        // fetch('https://jsonplaceholder.typicode.com/todos')
+        //     .then(res => res.json())
+        //     .then(res => {
+        //         console.log(res);
+        //         res?.length && setTodos(res);
+        //         setLoader(false);
+        //     });
     };
     const checkUpdated = (e, id) => {
-        let newTodo = todos.map(todo=>{
+        let newTodo = todos?.map(todo=>{
             if(todo.id==id) {
                 todo.completed=e.target.checked;
             }
@@ -28,23 +33,32 @@ const TodoApp = () => {
         setTodos(newTodo);
     }
     const onTodoChange = e => {
-        setNewTodoValue(e.target.value);
+        setNewTodo(e.target.value);
     };
     const onAddTodo = () => {
         setOpenInput(true);
     };
     const onSubmitTodo = e => {
         setOpenInput(false);
-        fetch('https://dummyjson.com/todos/add', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    todo: newTodoValue,
-                    completed: false,
-                    userId: 69,
-                })
-            })
-            .then(() => getData())
+        localStorage.setItem('todos', JSON.stringify([...todos,
+            {
+                title: newTodo,
+                completed: false,
+                userId: 1,
+                id: Math.floor(Math.random()*1000)
+            }]))
+        setNewTodo(null);
+        getData();
+        // fetch('https://dummyjson.com/todos/add', {
+        //         method: 'POST',
+        //         headers: { 'Content-Type': 'application/json' },
+        //         body: JSON.stringify({
+        //             todo: newTodo,
+        //             completed: false,
+        //             userId: 69,
+        //         })
+        //     })
+        //     .then(() => getData())
     }
     return(
         <section className="d-flex flex-column align-items-center">
@@ -56,7 +70,7 @@ const TodoApp = () => {
                         <span className="visually-hidden">Loading...</span>
                     </div>
                 }
-                {todos.length!=0 && todos.map((todo, index) => (
+                {todos?.length!=0 && todos?.map((todo, index) => (
                     <div key={index} className="todo-container d-flex justify-content-between">
                         <div className="todo-name">{todo.title}</div>
                         <div className="form-check">
@@ -66,7 +80,7 @@ const TodoApp = () => {
                 ))}
             </div>
             {openInput && <div className="input-group mb-3">
-                    <input type="text" value={newTodoValue} onChange={e=>onTodoChange(e)}className="form-control" placeholder="Enter new todo"/>
+                    <input type="text" value={newTodo} onChange={e=>onTodoChange(e)} className="form-control" placeholder="Enter new todo"/>
                     <button type="button" className="btn btn-primary btn-sm m-0 p-1" onClick={e=>onSubmitTodo(e)}>Add</button>
                 </div>
             }
